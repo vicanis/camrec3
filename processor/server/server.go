5576664c47dd114a6f8d132e76848fb78da2aa31
viktor <viktor@localhost>
2023-09-16 13:31:44 +0400

    refactor: processor: move server api handlers to /server/api package

--- processor/server/server.go
+++ processor/server/server.go
@@ -1,11 +1,8 @@
 package server
 
 import (
-	"fmt"
-	"log"
 	"net/http"
-	"processor/bundler"
-	"time"
+	"processor/server/api"
 
 	"github.com/gorilla/mux"
 )
@@ -13,47 +10,7 @@ import (
 func Start() error {
 	mx := mux.NewRouter()
 
-	apiRouter := mx.PathPrefix("/api").Subrouter()
-
-	apiRouter.Path("/play/{timestamp}").Methods(http.MethodGet).HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		timestamp := vars["timestamp"]
-
-		if err := renderPlayer(fmt.Sprintf("/event/%s", timestamp), w); err != nil {
-			log.Printf("template render failed: %s", err)
-		}
-	})
-
-	apiRouter.Path("/event/{timestamp}").Methods(http.MethodGet).HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		vars := mux.Vars(r)
-		timestamp := vars["timestamp"]
-
-		if timestamp == "" {
-			w.WriteHeader(http.StatusBadRequest)
-			w.Write([]byte("no timestamp"))
-			return
-		}
-
-		ts, err := time.ParseInLocation("20060102150405", timestamp, time.Local)
-		if err != nil {
-			w.WriteHeader(http.StatusBadRequest)
-			fmt.Fprintf(w, "invalid timestamp: %s", timestamp)
-			return
-		}
-
-		data, err := bundler.SearchVideoBundle(ts)
-		if err != nil {
-			w.WriteHeader(http.StatusNotFound)
-			return
-		}
-
-		w.Header().Add("Content-Type", "video/mp4")
-		w.Header().Add("Content-Length", fmt.Sprint(len(data)))
-
-		w.WriteHeader(http.StatusOK)
-
-		w.Write(data)
-	})
+	api.Configure(mx.PathPrefix("/api").Subrouter())
 
 	srv := &http.Server{
 		Addr:    ":80",
